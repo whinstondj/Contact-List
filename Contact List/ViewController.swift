dfa5236d793b4ce737664552c2bcadb23aaaf2a3
Winston <whinstondj@gmail.com>
2021-05-26 19:38:04 -0500

    Initial Commit

---
+++ Contact List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Contact List
+//
+//  Created by Winston Castillo on 26/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
